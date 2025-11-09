5ab09b91428ecef6f5f187f8a63b1e6a59078c26
Maxx Seminario <mseminario2@atlas.unl.edu>
2025-11-09 14:09:30 -0600

    ziscr inst test

--- hdl/MCU/tb/tb_defs.vhd
+++ hdl/MCU/tb/tb_defs.vhd
@@ -107,18 +107,18 @@ package tb_defs is
         -- "../rcf/xrv32uzbc-p-clmulh.rcf", -- Bit Manipulation - Carryless Mult Instructions
         -- "../rcf/xrv32uzbc-p-clmulr.rcf", -- Fail Genus 11/01/25
         -- "../rcf/xxrv32uzbc-p-clmul.rcf",
-        -- "../rcf/xxxrv32ziscr-p-csr.rcf", -- CSR Instructions (Custom)
-        -- "../rcf/xxxxxxperiph-p-NPU.rcf", -- Peripheral Tests
+        "../rcf/xxxrv32ziscr-p-csr.rcf", -- CSR Instructions (Custom)
+        "../rcf/xxxxxxperiph-p-NPU.rcf", -- Peripheral Tests
         "../rcf/xxxxperiph-p-SPIFM.rcf",
-        -- "../rcf/xxxxxxxspifem-p-lw.rcf",  
-        -- "../rcf/xxxxxxperiph-p-AFE.rcf",  
-        -- "../rcf/xxxperiph-p-SARADC.rcf",  
-        -- "../rcf/xxxxperiph-p-GPIO1.rcf",   
-        -- "../rcf/xxxxperiph-p-GPIO2.rcf",   
-        -- "../rcf/xxxxxperiph-p-UART.rcf", 
-        -- "../rcf/xxxperiph-p-SYSTEM.rcf", 
-        -- "../rcf/xxxxperiph-p-TIMER.rcf",
-        -- "../rcf/xxxrv32ziscr-p-csr.rcf"     -- CSR Instructions (Custom)    
+        "../rcf/xxxxxxxspifem-p-lw.rcf",  
+        "../rcf/xxxxxxperiph-p-AFE.rcf",  
+        "../rcf/xxxperiph-p-SARADC.rcf",  
+        "../rcf/xxxxperiph-p-GPIO1.rcf",   
+        "../rcf/xxxxperiph-p-GPIO2.rcf",   
+        "../rcf/xxxxxperiph-p-UART.rcf", 
+        "../rcf/xxxperiph-p-SYSTEM.rcf", 
+        "../rcf/xxxxperiph-p-TIMER.rcf",
+        "../rcf/xxxrv32ziscr-p-csr.rcf",     -- CSR Instructions (Custom)    
         "../rcf/xxxxxxperiph-p-SPI.rcf",  -- not tested here down
         "../rcf/xxxxperiph-p-SPISR.rcf", -- New SPI Slave test - SCK hf glitch in tb
         "../rcf/xxxxxrv32ua-p-lrsc.rcf" -- test not valid due to single core
